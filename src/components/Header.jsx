4bbb45fa8e555d8fe0417896f4a6e70c826e0790
Aditya Shukla <Shadowshukla76@gmail.com>
2025-04-26 19:32:49 +0530

    add some code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,77 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
+
 const Header = () => {
-    const gsap=useGSAP({})
+    const headerRef = useRef(null);
+    const coverRef = useRef(null)
+    const buttonSpanRef = useRef(null)
+
+    useGSAP(() => {
+        gsap.from(headerRef.current, {
+            opacity: 0,
+            y: -200,       // 👈 move from -200px above
+            duration: 1.5,  // 👈 shorter and snappier
+            ease: "expo.out" // 👈 expo out feels nice for entering
+        })
+
+        gsap.set(coverRef.current, {
+            scaleX: 0,
+            transformOrigin: "left center"
+        });
+    }, []);
+
+    const handleHover = () => {
+        const tl = gsap.timeline()
+        tl.to(coverRef.current, {
+            scaleX: 1,
+            duration: 1,
+            ease: "expo.inOut"
+        },"anim")
+        tl.to(buttonSpanRef.current, {
+            color: "black",
+            duration: 0.3,
+            ease: "power2.inOut"
+        },"anim")
+    }
+
+    const handleLeave = () => {
+        const tl = gsap.timeline()
+        tl.to(coverRef.current, {
+            scaleX: 0,
+            duration: 1,
+            ease: "expo.inOut"
+        },"anim")
+        tl.to(buttonSpanRef.current, {
+            color: "white",
+            duration: 0.3,
+            ease: "power2.inOut"
+        },"anim")
+
+    }
+
     return (
-        <header className='flex w-full z-[100] justify-between items-start'>
+        <header
+            ref={headerRef}
+            className='header flex w-full z-[100] justify-between items-start'
+        >
             <div className="flex flex-col items-start space-y-0.5">
                 <div className="h-24 w-24 overflow-hidden flex items-start justify-start">
                     <img
-                        className='object-contain object-top  h-full w-full filter invert'
+                        className='object-contain cursor-pointer object-top h-full w-full filter invert'
                         src='/MandrakeBlackLogoCropped.png'
                         alt='Mandrake'
                     />
                 </div>
             </div>
-            <button className='border border-white px-3   rounded-full flex items-center justify-center  md:px-6 py-2 '>
-                <span className='text-sm md:text-base text-white'>
+            <button onMouseEnter={handleHover} onMouseLeave={handleLeave} className='border relative border-white px-3 rounded-full overflow-hidden flex items-center justify-center md:px-6 py-2'>
+                <span ref={buttonSpanRef} className='text-sm md:text-base text-white z-20'>
                     Get In Touch
                 </span>
+                <div ref={coverRef} className='absolute top-0 left-0 bg-white w-full h-full rounded-full' />
             </button>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
